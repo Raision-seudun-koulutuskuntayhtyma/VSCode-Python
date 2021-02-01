03eb2b3547e7a7ef52ae519f79a302b25a48288d
Mika Vainio <koti.mika.vainio@gmail.com>
2021-02-01 07:53:35 +0200

    päivitety

--- testi1.py
+++ testi1.py
@@ -11,14 +11,3 @@ lista = [1, 3, 3, 6]
 monikko = (1, 5 ,5)
 joukko = {'a', 'x', 'z'}
 sanakirja = {'koira' : 'hund', 'kettu' : 'räv', 'karhu' : 'björn', 'virtahepo' : 'flodhäst'}
-
-def pallon_tilavuus(sade):
-    return pii * (sade ** 3)
-
-for x in range(6):
-    print(x)
-
-def FunctionName(args):
-    
-    
-       
