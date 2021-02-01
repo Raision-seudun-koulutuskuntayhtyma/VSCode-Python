a85bb5435595e7dc5b7a33ad6f2608d91d63da9c
Mika Vainio <koti.mika.vainio@gmail.com>
2021-02-01 07:52:09 +0200

    Update testi1.py

--- testi1.py
+++ testi1.py
@@ -1,8 +1,24 @@
 pii = 3.14159
-def piiri(halkaisija):
+def keha(halkaisija):
      return halkaisija * pii
 
 def ympyran_ala(halkaisija):
     return (halkaisija / 2)**2 * pii
 
-print(ympyran_ala(1))
+print('ympyrän ala on ', ympyran_ala(1))
+
+lista = [1, 3, 3, 6]
+monikko = (1, 5 ,5)
+joukko = {'a', 'x', 'z'}
+sanakirja = {'koira' : 'hund', 'kettu' : 'räv', 'karhu' : 'björn', 'virtahepo' : 'flodhäst'}
+
+def pallon_tilavuus(sade):
+    return pii * (sade ** 3)
+
+for x in range(6):
+    print(x)
+
+def FunctionName(args):
+    
+    
+       
