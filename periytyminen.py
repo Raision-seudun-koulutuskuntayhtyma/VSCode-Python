436cc8018f4b91b518891d3682d839a8983865f7
Mika Vainio <koti.mika.vainio@gmail.com>
2021-02-03 22:30:56 +0200

    päivitys

--- periytyminen.py
+++ periytyminen.py
@@ -1,16 +1,16 @@
-# Henkilö-luokka, jossa yleiset ihmiseen liittyvät perusominaisuudet -yliluokka
+# Henkilö-luokka, jossa yleiset ihmiseen liittyvät perusominaisuudet (yliluokka, parent, super class)
 class Henkilo:
     def __init__(self, etunimi, sukunimi):
         self.etunimi = etunimi
         self.sukunimi = sukunimi
 
-# Asiakas-luokka, joka perii Henkilö-luokan ominaisuudet ja jolla on erillinen asiakasnumero-ominaisuus -aliluokka
+# Asiakas-luokka, joka perii Henkilö-luokan ominaisuudet ja jolla on erillinen asiakasnumero-ominaisuus (aliluokka, child sub class)
 class Asiakas(Henkilo):
     def __init__(self, etunimi, sukunimi, asiakasnumero):
         super().__init__(etunimi, sukunimi)
         self.asiakasnumero = asiakasnumero
         
-# Myyjä-luokka, perii Henkilöluokan ominaisuudet, erityispiirteenä autoetu -aliluokka
+# Myyjä-luokka, perii Henkilöluokan ominaisuudet, erityispiirteenä autoetu (aliluokka, child, sub class)
 class Myyja(Henkilo):
     def __init__(self, etunimi, sukunimi, autoetu):
         super().__init__(etunimi, sukunimi)
